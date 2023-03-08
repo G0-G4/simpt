cefd9377d1fb9c8b09ca9442268e047ae5a59c8d
George Grishchenkov <goscha-grishenkov@yandex.ru>
2023-03-08 23:12:41 +0300

    add test

--- backend/simpt/tracker/tests.py
+++ backend/simpt/tracker/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import json
+
 
 # Create your tests here.
+class RegisterTest(TestCase):
+    def test_register(self):
+        user = {
+            "username": "user1",
+            "email": "user1@mail.ru",
+            "first_name": "first name",
+            "last_name": "last name"
+        }
+        password = {
+            "password": "user1user1",
+            "password2": "user1user1",
+        }
+        c = Client()
+        response = c.post('/users/register/', user | password)
+        self.assertJSONEqual(json.dumps(user), json.dumps(response.json()))
